4dfa8742b458ec9aba99afb0c9fc00f5b5e8eeaf
Cary Huang <hcary328@gmail.com>
2025-03-25 15:36:56 -0700

    script

--- ci/build-synchdb.sh
+++ ci/build-synchdb.sh
@@ -22,15 +22,17 @@ function build_synchdb()
 {
 	pg_major="$1"
 
-	sudo apt install -y postgresql-common
-	sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
-	sudo apt install -y postgresql-${pg_major} postgresql-server-dev-${pg_major}
+	#sudo apt install -y postgresql-common
+	#sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh
+	#sudo apt install -y postgresql-${pg_major} postgresql-server-dev-${pg_major}
 
 	builddir="${basedir}/build-${pg_major}"
 	echo "Beginning build for PostgreSQL ${pg_major}..." >&2
 	mkdir -p "${builddir}" && cd "${builddir}"
 	CFLAGS=-Werror "${basedir}/configure" PG_CONFIG="/usr/lib/postgresql/${pg_major}/bin/pg_config" --enable-coverage --with-security-flags
 	installdir="${builddir}/install"	
+	
+	make -j$(nproc) build_dbz && mkdir -p "${installdir}" && { make DESTDIR="${installdir}" install-all || make DESTDIR="${installdir}" install ; }
 
 }
 
