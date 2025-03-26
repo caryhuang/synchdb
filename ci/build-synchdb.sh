cc71051fb6c3c8be0a61bf84dad29bfa85af4fde
Cary Huang <hcary328@gmail.com>
2025-03-26 16:02:23 -0700

    script

--- ci/build-synchdb.sh
+++ ci/build-synchdb.sh
@@ -30,8 +30,8 @@ function build_synchdb()
 	make build_dbz PG_CONFIG=/usr/lib/postgresql/${pg_major}/bin/pg_config
 	make PG_CONFIG=/usr/lib/postgresql/${pg_major}/bin/pg_config
 
-	sudo USE_PGXS=1 make install_dbz DESTDIR=${installdir}  PG_CONFIG=/usr/lib/postgresql/${pg_major}/bin/pg_config
 	sudo USE_PGXS=1 make install DESTDIR=${installdir} PG_CONFIG=/usr/lib/postgresql/${pg_major}/bin/pg_config
+	sudo USE_PGXS=1 make install_dbz libdir==${installdir}/usr/lib/postgresql/${pg_major}/lib  PG_CONFIG=/usr/lib/postgresql/${pg_major}/bin/pg_config
 
 	cd $installdir
 	ls
