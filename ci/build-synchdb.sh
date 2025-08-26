b97df3014c33b6d01a818431bdd0f372ad4aaf8a
Cary Huang <hcary328@gmail.com>
2025-08-26 16:05:50 -0700

    build update

--- ci/build-synchdb.sh
+++ ci/build-synchdb.sh
@@ -6,6 +6,7 @@ IFS=$'\n\t'
 
 # read pg major version, error if not provided
 PG_MAJOR=${PG_MAJOR:?please provide the postgres major version}
+PG_BRANCH=${PG_BRANCH:?please provide the postgres branch}
 
 # get codename from release file
 . /etc/os-release
@@ -19,16 +20,21 @@ function build_synchdb()
 {
 	pg_major="$1"
 
+	echo $PG_BRANCH
 	installdir="${basedir}/synchdb-install-${pg_major}"
 	mkdir -p $installdir
 	echo "Beginning build for PostgreSQL ${pg_major}..." >&2
-	#mkdir -p "${builddir}" && cd "${builddir}"
-	export USE_PGXS=1
-	make build_dbz PG_CONFIG=/usr/lib/postgresql/${pg_major}/bin/pg_config
-	make PG_CONFIG=/usr/lib/postgresql/${pg_major}/bin/pg_config
 
-	sudo USE_PGXS=1 make install DESTDIR=${installdir} PG_CONFIG=/usr/lib/postgresql/${pg_major}/bin/pg_config
-	sudo USE_PGXS=1 make install_dbz pkglibdir=${installdir}/usr/lib/postgresql/${pg_major}/lib  PG_CONFIG=/usr/lib/postgresql/${pg_major}/bin/pg_config
+	exit 1
+	#mkdir -p "${builddir}" && cd "${builddir}"
+	
+	
+	#export USE_PGXS=1
+	#make build_dbz PG_CONFIG=/usr/lib/postgresql/${pg_major}/bin/pg_config
+	#make PG_CONFIG=/usr/lib/postgresql/${pg_major}/bin/pg_config
+
+	#sudo USE_PGXS=1 make install DESTDIR=${installdir} PG_CONFIG=/usr/lib/postgresql/${pg_major}/bin/pg_config
+	#sudo USE_PGXS=1 make install_dbz pkglibdir=${installdir}/usr/lib/postgresql/${pg_major}/lib  PG_CONFIG=/usr/lib/postgresql/${pg_major}/bin/pg_config
 
 	cd $installdir
 	ls
