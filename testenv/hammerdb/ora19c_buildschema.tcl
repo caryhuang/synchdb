774e5e6d5d80c0ce19ae839042ccb2a61900ddac
Cary Huang <hcary328@gmail.com>
2025-09-25 13:59:04 -0700

    missing tpcc test scripts

---
+++ testenv/hammerdb/ora19c_buildschema.tcl
@@ -0,0 +1,11 @@
+dbset db ora
+diset connection system_user DBZUSER
+diset connection system_password dbz
+diset connection instance "ora19c:1521/FREE"
+diset tpcc tpcc_user DBZUSER
+diset tpcc tpcc_pass dbz
+diset tpcc tpcc_def_tab LOGMINER_TBS
+
+buildschema
+exit
+
